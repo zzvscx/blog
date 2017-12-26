180e411463a33c140726b40f527c1fb257af652c
zsw <zsw18049252607@163.com>
2017-12-26 14:58:11 +0800

    修改前端模板

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,19 @@
-from ..models import Post
+from django import template
+from ..models import Post, Category
 
+register = template.Library()
+
+#获取最新的文章
+@register.simple_tag
 def get_recent_posts(num=5):
-    return Post.objects.all().order_by('-created_at')[:num]
+    return Post.objects.all().order_by('-created_time')[:num]
+
+#将文章按月份归档
+@register.simple_tag
+def archives():
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+#类别
+@register.simple_tag
+def get_categories():
+    return Category.objects.all()
