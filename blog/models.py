745d89d8e1a968370cade490c6338782f6b29d16
zsw <zsw18049252607@163.com>
2017-12-28 16:24:04 +0800

    修改细节

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 #-*- coding: utf-8 -*-
+import markdown
+from django.utils.html import strip_tags
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -30,6 +32,7 @@ class Post(models.Model):
     category = models.ForeignKey(Category, verbose_name='类型')
     tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
     author = models.ForeignKey(User, verbose_name='作者')
+    views = models.PositiveIntegerField(verbose_name='浏览量',default=0) #PositiveIntegerField 正整数
     created_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     modified_time = models.DateTimeField(verbose_name='最后修改时间', auto_now=True)
     
@@ -38,7 +41,24 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_time']
-
+    
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+    
+    @property
+    def comment_number(self):
+        return self.comment_set.all().count
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def save(self,*args,**kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            #strip_tags 删除html标记
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args,**kwargs)
